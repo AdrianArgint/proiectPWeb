cacc4e06541c4bf3f5df60388e8a82c9d0ff5276
Adrian <Adrian@DESKTOP-0PG6PFA>
2022-05-21 00:00:14 +0300

    Hot fix 3

--- proiect/webpack/webpack.custom.js
+++ proiect/webpack/webpack.custom.js
@@ -49,14 +49,7 @@ module.exports = async (config, options, targetOptions) => {
               heartbeatTimeout: 60000,
             },
           },
-          /*
-          ghostMode: { // uncomment this part to disable BrowserSync ghostMode; https://github.com/jhipster/generator-jhipster/issues/11116
-            clicks: false,
-            location: false,
-            forms: false,
-            scroll: false,
-          },
-          */
+
         },
         {
           reload: targetOptions.target === 'build', // enabled for build --watch
@@ -89,7 +82,6 @@ module.exports = async (config, options, targetOptions) => {
       to: 'swagger-ui/',
     },
     { from: './src/main/webapp/swagger-ui/', to: 'swagger-ui/' },
-    // jhipster-needle-add-assets-to-webpack - JHipster will add/remove third-party resources in this array
   ];
 
   if (patterns.length > 0) {
@@ -101,17 +93,12 @@ module.exports = async (config, options, targetOptions) => {
       // APP_VERSION is passed as an environment variable from the Gradle / Maven build tasks.
       __VERSION__: JSON.stringify(environment.__VERSION__),
       __DEBUG_INFO_ENABLED__: environment.__DEBUG_INFO_ENABLED__ || config.mode === 'development',
-      // The root URL for API calls, ending with a '/' - for example: `"https://www.jhipster.tech:8081/myservice/"`.
-      // If this URL is left empty (""), then it will be relative to the current context.
-      // If you use an API server, in `prod` mode, you will need to enable CORS
-      // (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
       SERVER_API_URL: JSON.stringify(environment.SERVER_API_URL),
     })
   );
 
   config = merge(
     config
-    // jhipster-needle-add-webpack-config - JHipster will add custom config
   );
 
   return config;

44088a5cb66db124937ae594cae7b2a74ddf98a3
Adrian <Adrian@DESKTOP-0PG6PFA>
2022-05-20 23:59:35 +0300

    Hot fix 2

--- proiect/src/main/webapp/app/login/login.service.ts
+++ proiect/src/main/webapp/app/login/login.service.ts
@@ -9,8 +9,6 @@ export class LoginService {
   constructor(private location: Location, private authServerProvider: AuthServerProvider) {}
 
   login(): void {
-    // If you have configured multiple OIDC providers, then, you can update this URL to /login.
-    // It will show a Spring Security generated login page with links to configured OIDC providers.
     location.href = `${location.origin}${this.location.prepareExternalUrl('oauth2/authorization/oidc')}`;
   }
 
